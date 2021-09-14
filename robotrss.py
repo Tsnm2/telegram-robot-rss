68e63b83d7f45f95bb01c86f3acf095a0afd50e1
Tsnm2 <90688335+Tsnm2@users.noreply.github.com>
2021-09-15 00:07:31 +0530

    Update robotrss.py

--- robotrss.py
+++ robotrss.py
@@ -145,8 +145,9 @@ class RobotRss(object):
 
         entries = FeedHandler.parse_feed(url[0], args_count)
         for entry in entries:
-            message = "[" + url[1] + "] <a href='" + \
-                entry.link + "'>" + entry.title + "</a>"
+            message = "/mirror + \ 
+               entry.link + entry.title"
+                
             print(message)
 
             try:
